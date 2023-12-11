ccdb17a57773400ad81f961eea0711c67a26ac4e
zaber04 <zaber.mahbub@gmail.com>
2023-12-11 19:05:46 +0600

    1. exploring cross microservice library import (unstable version)

---
+++ server/ip-handler/app/Http/Controllers/IpHandlerController.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\JsonResponse;
+use GatewayNamespace\Traits\ApiResponse;
+
+class IpHandlerController extends BaseController
+{
+    use ApiResponse;
+
+    // Controller methods...
+}
