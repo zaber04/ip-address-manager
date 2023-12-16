06b60b000d3fbfffd1e1129ad9f9d9fe2d2cc2ea
Kazi Zaber <zaber.mahbub@gmail.com>
2023-12-16 17:18:52 +0600

    clean angular setup files

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'client';
+	title = 'client';
+
+	loginUI = false;
+	sidebar = false;
+
+	constructor(
+		private router: Router,
+		// private sharedService: SharedService,
+	) {
+		this.router.events.subscribe(event => {
+			if (event instanceof NavigationStart) {
+				this.loginUI = false;
+
+				if (event.url === '/login') {
+					this.loginUI = true;
+				}
+			}
+		});
+	}
 }
