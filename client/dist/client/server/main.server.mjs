77b571d4e6ef6db9eddfd08f84059c82a3d128e1
Kazi Zaber <zaber.mahbub@gmail.com>
2023-12-19 05:08:59 +0600

    include the asset files

---
+++ client/dist/client/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-MBC7ZU2B.mjs";import"./chunk-DQ5D3PB5.mjs";import"./chunk-UAN5GN32.mjs";import"./chunk-ON4X33R7.mjs";import"./chunk-TOYUHSAU.mjs";import"./chunk-OPECEUMY.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
